5c68e3a42985e36fb1e50bb06e7238ab8717590a
TomoKanazawa <itstomohiro@gmail.com>
2025-04-14 21:15:26 -0700

    Deprecate insert_one and insert_many

--- src/client/Collection.ts
+++ src/client/Collection.ts
@@ -208,7 +208,7 @@ export class Collection {
     }
   }
 
-  public async insertMany(documents: unknown[]): Promise<unknown> {
+  public async insert(documents: unknown[]): Promise<unknown> {
     const url = this.getCollectionUrl();
     const headers = this.getHeaders();
     const serializedDocs = documents.map((doc) => this.serialize(doc));
@@ -221,21 +221,6 @@ export class Collection {
 
     return this.handleResponse(response);
   }
-
-  public async insertOne(document: unknown): Promise<unknown> {
-    const url = this.getCollectionUrl();
-    const headers = this.getHeaders();
-    const serializedDoc = this.serialize(document);
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers,
-      body: JSON.stringify({ documents: [serializedDoc] }),
-    });
-
-    return this.handleResponse(response);
-  }
-
 
   public async update(
     filter: unknown,
